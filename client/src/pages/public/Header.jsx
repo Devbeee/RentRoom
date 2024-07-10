eba59ce31f5250841fdbcb7d236c9d4b1b7191a0
Devbeee <tngb1002@gmail.com>
2024-07-11 01:21:36 +0700

    Finish UI login and register

--- client/src/pages/public/Header.jsx
+++ client/src/pages/public/Header.jsx
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import logo from "../../assets/image/logo-home.png";
 import icons from "../../utils/icons";
 import { path } from "../../utils/constant";
@@ -8,29 +8,32 @@ import { Button } from "../../components";
 const { AiOutlinePlusCircle } = icons;
 function Header() {
   const navigate = useNavigate();
-  const goLogin = useCallback(() => {
-    navigate(path.LOGIN);
+  const goLogin = useCallback((flag) => {
+    navigate(path.LOGIN, { state: { flag } });
   }, []);
   return (
     <div className="w-1100">
       <div className="w-full flex items-center justify-between">
-        <img
-          src={logo}
-          alt="logo"
-          className="w-[240px] h-[70px] object-contain"
-        />
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[240px] h-[70px] object-contain"
+          />
+        </Link>
         <div className="flex items-center gap-1">
           <small>Phongtro123.com xin chào</small>
           <Button
             text="Đăng ký"
             textColor="text-white"
             bgColor="bg-[#3961fb]"
+            onClick={() => goLogin(true)}
           />
           <Button
             text="Đăng nhập"
             textColor="text-white"
             bgColor="bg-[#3961fb]"
-            onClick={goLogin}
+            onClick={() => goLogin(false)}
           />
           <Button
             text="Đăng tin mới"
--- client/src/pages/public/Login.jsx
+++ client/src/pages/public/Login.jsx
@@ -1,21 +1,54 @@
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { InputForm, Button } from "../../components";
 function Login() {
+  const location = useLocation();
+  const [isRegister, setIsRegister] = useState(location.state?.flag);
+  useEffect(() => {
+    const currentPathname = location.state?.flag ? "/register" : "/login";
+    window.history.pushState({}, "", currentPathname);
+    setIsRegister(location.state?.flag);
+  }, [location.state?.flag]);
   return (
     <div className="bg-white w-[600px] mw-600 p-[30px] pb-[100px] rounded-md shadow-sm mt-3">
-      <h3 className="font-semibold text-2xl mb-3">Đăng nhập</h3>
+      <h3 className="font-semibold text-2xl mb-3">
+        {isRegister ? "Đăng ký tài khoản" : "Đăng nhập"}
+      </h3>
       <div className="w-full flex flex-col gap-5">
+        {isRegister && <InputForm label={"HỌ TÊN"} />}
         <InputForm label={"SỐ ĐIỆN THOẠI"} />
         <InputForm label={"MẬT KHẨU"} />
         <Button
-          text={"Đăng nhập"}
+          text={isRegister ? "Đăng ký" : "Đăng nhập"}
           bgColor={"bg-secondary"}
           textColor={"text-white"}
           fullWidth
         />
       </div>
       <div className="mt-7 flex items-center justify-between">
-        <small className="text-[blue] hover:text-[red] cursor-pointer">Bạn quên mật khẩu</small>
-        <small className="text-[blue] hover:text-[red] cursor-pointer">Tạo tài khoản mới</small>
+        {isRegister ? (
+          <small>
+            Bạn đã có tài khoản?
+            <span
+              className="text-blue-500 hover:underline cursor-pointer ml-1"
+              onClick={() => setIsRegister(false)}
+            >
+              Đăng nhập ngay
+            </span>
+          </small>
+        ) : (
+          <>
+            <small className="text-[blue] hover:text-[red] cursor-pointer">
+              Bạn quên mật khẩu
+            </small>
+            <small
+              className="text-[blue] hover:text-[red] cursor-pointer"
+              onClick={() => setIsRegister(true)}
+            >
+              Tạo tài khoản mới
+            </small>
+          </>
+        )}
       </div>
     </div>
   );
